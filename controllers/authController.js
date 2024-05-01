cea6c24a1c9fe2a9cc774dc448a7eb5b74980c19
harnillathiya <harnillathiya07@gmail.com>
2024-05-01 14:54:18 +0530

    jwttokenadd

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,30 +1,9 @@
+import jwt from 'jsonwebtoken';
 import User from "../Models/User.js";
-import bcrypt from "bcryptjs";
-
-export const register = async (req, res) => {
-  try {
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password, salt);
-
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: hash,
-    });
-    await newUser.save();
-    res
-      .status(200)
-      .json({ success: true, message: "Account Created Successfully" });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ success: false, message: "Failed to Create . Try Again" });
-  }
-};
-
 
 export const login = async (req, res) => {
   const email = req.body.email;
+  const password = req.body.password;
   try {
     const user = await User.findOne({ email });
 
@@ -38,7 +17,13 @@ export const login = async (req, res) => {
         .status(401)
         .json({ success: false, message: "Incorrect credentials" });
     }
-    return res.status(200).json({ success: true, message: "Login successful", user });
+    const token = jwt.sign({
+      userId: user._id
+    },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' });
+
+    return res.status(200).json({ success: true, message: "Login successful", user, token });
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: error.message });
